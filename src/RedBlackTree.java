514304157ee33c2f0866e630b5064448114e0226
Pavel Votyakov <stix.brsk@mail.ru>
2013-03-03 17:54:12 +0600

    Done

--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -20,15 +20,15 @@ public class RedBlackTree<K extends Comparable<K>, V extends Comparable<V>> impl
      */
 
     private Node<K, V> root = null;
-    enum Color { red, black }
+
+    enum Color {red, black}
+
     private int m_size;
 
     static private Node NIL;
 
-    public RedBlackTree()
-    {
-        NIL = new Node<K,V>();
-
+    public RedBlackTree() {
+        NIL = new Node();
         root = NIL;
     }
 
@@ -42,7 +42,7 @@ public class RedBlackTree<K extends Comparable<K>, V extends Comparable<V>> impl
 //        NIL.left = NIL;
 //        NIL.right = NIL;
 
-        private Color color=Color.black;
+        private Color color = Color.black;
 
 
         Node(K key, V value) {
@@ -261,66 +261,19 @@ public class RedBlackTree<K extends Comparable<K>, V extends Comparable<V>> impl
         return null;
     }
 
-    /**
-     * Removes the mapping for a key from this map if it is present
-     * (optional operation).   More formally, if this map contains a mapping
-     * from key <tt>k</tt> to value <tt>v</tt> such that
-     * <code>(key==null ?  k==null : key.equals(k))</code>, that mapping
-     * is removed.  (The map can contain at most one such mapping.)
-     * <p/>
-     * <p>Returns the value to which this map previously associated the key,
-     * or <tt>null</tt> if the map contained no mapping for the key.
-     * <p/>
-     * <p>If this map permits null values, then a return value of
-     * <tt>null</tt> does not <i>necessarily</i> indicate that the map
-     * contained no mapping for the key; it's also possible that the map
-     * explicitly mapped the key to <tt>null</tt>.
-     * <p/>
-     * <p>The map will not contain a mapping for the specified key once the
-     * call returns.
-     *
-     * @return the previous value associated with <tt>key</tt>, or
-     *         <tt>null</tt> if there was no mapping for <tt>key</tt>.
-     * @throws UnsupportedOperationException if the <tt>remove</tt> operation
-     *                                       is not supported by this map
-     * @throws ClassCastException            if the key is of an inappropriate type for
-     *                                       this map
-     *                                       (<a href="Collection.html#optional-restrictions">optional</a>)
-     * @throws NullPointerException          if the specified key is null and this
-     *                                       map does not permit null keys
-     *                                       (<a href="Collection.html#optional-restrictions">optional</a>)
-     */
-    /**
-     * Copies all of the mappings from the specified map to this map
-     * (optional operation).  The effect of this call is equivalent to that
-     * of calling {@link #n(Object, Object) put(k, v)} on this map once
-     * for each mapping from key <tt>k</tt> to value <tt>v</tt> in the
-     * specified map.  The behavior of this operation is undefined if the
-     * specified map is modified while the operation is in progress.
-     *
-     * @param m mappings to be stored in this map
-     * @throws UnsupportedOperationException if the <tt>putAll</tt> operation
-     *                                       is not supported by this map
-     * @throws ClassCastException            if the class of a key or value in the
-     *                                       specified map prevents it from being stored in this map
-     * @throws NullPointerException          if the specified map is null, or if
-     *                                       this map does not permit null keys or values, and the
-     *                                       specified map contains null keys or values
-     * @throws IllegalArgumentException      if some property of a key or value in
-     *                                       the specified map prevents it from being stored in this map
-     */
+
     @Override
-    public V remove(Object K)  {
+    public V remove(Object K) {
         if (K == null) throw new NullPointerException();
         Node<K, V> current = root;
         Node<K, V> myNode = NIL;
+        Node<K,V> node;
         K k = (K) K;
         while (current != NIL) {
             int cmp = k.compareTo(current.key);
             if (cmp == 0) {
                 break;
             } else {
-                myNode = current;
                 if (cmp < 0) {
                     current = current.left;
                 } else {
@@ -333,35 +286,39 @@ public class RedBlackTree<K extends Comparable<K>, V extends Comparable<V>> impl
         }
         m_size--;
         V returnValue = current.value;
-        if (current.right == NIL) {
-            if (myNode == NIL) {
-                root = current.left;
-            } else {
-                if (current == myNode.left) {
-                    myNode.left = current.left;
-                } else {
-                    myNode.right = current.left;
-                }
-            }
+
+        if (current==null || current==NIL) return null;
+        if (current.left == NIL ||current.right == NIL) {
+            myNode = current;
         } else {
-            Node<K, V> rightMin = current.right;
-            myNode = NIL;
-            while (rightMin.left != NIL) {
-                myNode = rightMin;
-                rightMin = rightMin.left;
-            }
-            if (myNode != NIL) {
-                myNode.left = rightMin.right;
-            } else {
-                current.right = rightMin.right;
-            }
-            current.key = rightMin.key;
-            current.value = rightMin.value;
+            myNode = current.right;
+            while (myNode.left != NIL) myNode = myNode.left;
         }
-        removeFix(current);
+        if (myNode.left != NIL)
+            node = myNode.left;
+        else
+            node = myNode.right;
+        node.parent = myNode.parent;
+        if (myNode.parent!=null)
+        {
+            if (myNode==myNode.parent.left)
+            {
+                myNode.parent.left = node;
+            }
+            else myNode.parent.right = node;
+        }
+        else root = node;
+        if (myNode!=current)
+        {
+            current.key = myNode.key;
+            current.value = myNode.value;
+        }
+        if (myNode.color==Color.black)
+        {
+            removeFix(node);
+        }
         return returnValue;
     }
-
 
 
     public void putAll(Map<? extends K, ? extends V> m) {
@@ -378,7 +335,7 @@ public class RedBlackTree<K extends Comparable<K>, V extends Comparable<V>> impl
     @Override
     public void clear() {
         root = null;
-        m_size=0;
+        m_size = 0;
     }
 
     /**
@@ -442,67 +399,58 @@ public class RedBlackTree<K extends Comparable<K>, V extends Comparable<V>> impl
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    private void rotateRight(Node<K,V> x)
-    {
-        Node<K,V> y = x.left;
+    private void rotateRight(Node<K, V> x) {
+        Node<K, V> y = x.left;
         x.left = y.right;
-        if (y.right!=null) y.right.parent=x;
-        if (y != null) y.parent = x.parent;
-        if (x.parent != null)  {
-            if (x==x.parent.right)
-                x.parent.right=y;
+        if (y.right != NIL) y.right.parent = x;
+        if (y != NIL) y.parent = x.parent;
+        if (x.parent != null) {
+            if (x == x.parent.right)
+                x.parent.right = y;
             else
-                x.parent.left=y;
-        }   else {
-            root=y;
+                x.parent.left = y;
+        } else {
+            root = y;
         }
 
         y.right = x;
-        if (x != null) x.parent = y;
+        if (x != NIL) x.parent = y;
     }
-    private void rotateLeft(Node<K,V> x)
-    {
-        Node<K,V> y = x.right;
+
+    private void rotateLeft(Node<K, V> x) {
+        Node<K, V> y = x.right;
         x.right = y.left;
-        if (y.left!=null) y.left.parent=x;
-        if (y != null) y.parent = x.parent;
-        if (x.parent != null)  {
-            if (x==x.parent.left)
-                x.parent.left=y;
+        if (y.left != NIL) y.left.parent = x;
+        if (y != NIL) y.parent = x.parent;
+        if (x.parent != null) {
+            if (x == x.parent.left)
+                x.parent.left = y;
             else
-                x.parent.right=y;
-        }   else {
-            root=y;
+                x.parent.right = y;
+        } else {
+            root = y;
         }
 
         y.left = x;
-        if (x != null) x.parent = y;
+        if (x != NIL) x.parent = y;
     }
 
-    private void insertFix(Node<K,V> x)
-    {
+    private void insertFix(Node<K, V> x) {
         x.color = Color.red;
-        while (x != null && x!=root && x.parent.color==Color.red )
-        {
-            if (x.parent == x.parent.parent.left)
-            {
+        while (x != null && x != root && x.parent.color == Color.red) {
+            if (x.parent == x.parent.parent.left) {
                 //мы слева
-                Node<K,V> y = x.parent.parent.right;
-                if (y.color==Color.red)
-                {
+                Node<K, V> y = x.parent.parent.right;
+                if (y.color == Color.red) {
                     //дядя справа красный
                     y.color = Color.black;
                     x.parent.color = Color.black;
                     x.parent.parent.color = Color.red;
                     x = x.parent.parent;
-                }
-                else
-                {
-
+                } else {
                     //дядя справа чёрный
-                    if (x.parent.right == x)
-                    {
-                        x=x.parent;
+                    if (x.parent.right == x) {
+                        x = x.parent;
                         rotateLeft(x);
                     }
                     x.parent.color = Color.black;
@@ -510,29 +458,21 @@ public class RedBlackTree<K extends Comparable<K>, V extends Comparable<V>> impl
                     rotateRight(x.parent.parent);
                 }
 
-            }
-            else
-            {
-                Node<K,V> y = x.parent.parent.left;
-                if (y.color==Color.red)
-                {
+            } else {
+                Node<K, V> y = x.parent.parent.left;
+                if (y.color == Color.red) {
                     y.color = Color.black;
                     x.parent.color = Color.black;
                     x.parent.parent.color = Color.red;
                     x = x.parent.parent;
-                }
-                else
-                {
-                    if (x.parent.left == x)
-                    {
-                        x=x.parent;
+                } else {
+                    if (x.parent.left == x) {
+                        x = x.parent;
                         rotateRight(x);
                     }
                     x.parent.color = Color.black;
                     x.parent.parent.color = Color.red;
                     rotateLeft(x.parent.parent);
-
-
 
 
                 }
@@ -545,15 +485,17 @@ public class RedBlackTree<K extends Comparable<K>, V extends Comparable<V>> impl
         root.color = Color.black;
     }
 
-    private void removeFix(Node<K,V> x)
-    {
-        while (x != root && x.color == Color.black && x!=NIL) {
+
+
+
+    private void removeFix(Node<K, V> x) {
+        while (x != root && x.color == Color.black) {
             if (x == x.parent.left) {
-                Node<K,V> myNode = x.parent.right;
-                if (x.color == Color.red) {
+                Node<K, V> myNode = x.parent.right;
+                if (myNode.color == Color.red) {
                     myNode.color = Color.black;
                     x.parent.color = Color.red;
-                    rotateLeft (x.parent);
+                    rotateLeft(x.parent);
                     myNode = x.parent.right;
                 }
                 if (myNode.left.color == Color.black && myNode.right.color == Color.black) {
@@ -563,13 +505,13 @@ public class RedBlackTree<K extends Comparable<K>, V extends Comparable<V>> impl
                     if (myNode.right.color == Color.black) {
                         myNode.left.color = Color.black;
                         myNode.color = Color.red;
-                        rotateRight (myNode);
+                        rotateRight(myNode);
                         myNode = x.parent.right;
                     }
                     myNode.color = x.parent.color;
                     x.parent.color = Color.black;
                     myNode.right.color = Color.black;
-                    rotateLeft (x.parent);
+                    rotateLeft(x.parent);
                     x = root;
                 }
             } else {
@@ -577,7 +519,7 @@ public class RedBlackTree<K extends Comparable<K>, V extends Comparable<V>> impl
                 if (myNode.color == Color.red) {
                     myNode.color = Color.black;
                     x.parent.color = Color.red;
-                    rotateRight (x.parent);
+                    rotateRight(x.parent);
                     myNode = x.parent.left;
                 }
                 if (myNode.right.color == Color.black && myNode.left.color == Color.black) {
@@ -587,13 +529,13 @@ public class RedBlackTree<K extends Comparable<K>, V extends Comparable<V>> impl
                     if (myNode.left.color == Color.black) {
                         myNode.right.color = Color.black;
                         myNode.color = Color.red;
-                        rotateLeft (myNode);
+                        rotateLeft(myNode);
                         myNode = x.parent.left;
                     }
                     myNode.color = x.parent.color;
                     x.parent.color = Color.black;
                     myNode.left.color = Color.black;
-                    rotateRight (x.parent);
+                    rotateRight(x.parent);
                     x = root;
                 }
             }
--- src/mapTest.java
+++ src/mapTest.java
@@ -14,7 +14,7 @@ import java.util.TreeMap;
  */
 
 public class mapTest extends Assert {
-    private Map<Integer, Integer> testMap = new RedBlackTree<Integer, Integer>();
+    private RedBlackTree<Integer, Integer> testMap = new RedBlackTree<Integer, Integer>();
     private Map<Integer, Integer> controlMap = new TreeMap<Integer, Integer>();
     Integer[] values;
     Integer[] keys;
@@ -22,12 +22,12 @@ public class mapTest extends Assert {
     @Before
     public void generateData() {
         Random random = new Random();
-        int size = 100;
+        int size = 1000;
         values = new Integer[size];
         keys = new Integer[size];
         for (int i = 0; i < size; i++) {
 
-            keys[i] = random.nextInt();
+            keys[i] = random.nextInt(100);
             values[i] = random.nextInt();
         }
     }
@@ -35,7 +35,8 @@ public class mapTest extends Assert {
     private void checkMap(Map<Integer, Integer> a, Map<Integer, Integer> b) {
         assertEquals("Size", a.size(), b.size());
         for (Integer key : b.keySet()) {
-            assertEquals("Value for key " + key.toString(), b.get(key),a.get(key));
+            assertEquals("Value for key " + key.toString(), b.get(key), a.get(key));
+
         }
 
 
@@ -52,19 +53,32 @@ public class mapTest extends Assert {
     }
 
     @Test
-    public void delTest() {
-
-       putNode(1);
+    public void delTest1()
+    {
+        putNode(95);
         putNode(2);
-        putNode(3);
-        putNode(4);
-        putNode(5);
-        putNode(6);
-        putNode(7);
+        putNode(27);
+        putNode(60);
+        putNode(62);
 
-        delNode(3);
+        delNode(95);
+        delNode(2);
+        checkMap(testMap,controlMap);
+    }
 
+    @Test
+    public void delTest() {
+        for (int i = 0; i < keys.length; i++) {
+            testMap.put(keys[i], values[i]);
+            controlMap.put(keys[i], values[i]);
+        }
+
+        for (int i = 0; i < keys.length; i++) {
+            testMap.remove(keys[i]);
+            controlMap.remove(keys[i]);
             checkMap(testMap, controlMap);
+        }
+//
 
     }
 
